3137efa9f9055a4c91d75094d4ad92fdd0c0d418
Cafe137 <xcafe137@gmail.com>
2024-10-18 12:39:43 +0300

    fix: convert mantaray reference to string

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
+import { Bee } from '@ethersphere/bee-js'
+import { BEE_API, STAMP } from './globals'
 import { createGsocListener } from './listener'
+import { createMantarayWithMetadata } from './mantaray'
 
 main()
 
 async function main() {
+  const bee = new Bee(BEE_API)
+  console.log(await createMantarayWithMetadata(bee, STAMP, 'secretkey', 'secretvalue'))
   if (0) {
     createGsocListener()
   }
--- src/mantaray.ts
+++ src/mantaray.ts
@@ -1,19 +1,21 @@
 import { Bee } from '@ethersphere/bee-js'
 import { Binary } from 'cafe-utility'
-import { Bytes, MantarayNode, Reference } from 'mantaray-js'
+import { Bytes, MantarayNode } from 'mantaray-js'
 
 export async function createMantarayWithMetadata(
   bee: Bee,
   stamp: string,
   key: string,
   value: string,
-): Promise<Reference> {
+): Promise<string> {
   const mantaray = new MantarayNode()
   mantaray.setMetadata = {
     [key]: value,
   }
-  return mantaray.save(async data => {
-    const uploadDataResult = await bee.uploadData(stamp, data)
-    return Binary.hexToUint8Array(uploadDataResult.reference) as Bytes<32>
-  })
+  return Binary.uint8ArrayToHex(
+    await mantaray.save(async data => {
+      const uploadDataResult = await bee.uploadData(stamp, data)
+      return Binary.hexToUint8Array(uploadDataResult.reference) as Bytes<32>
+    }),
+  )
 }
