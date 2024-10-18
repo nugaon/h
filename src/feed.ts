baf8a874fd5df76d41ba8844caf336d269a92190
Cafe137 <xcafe137@gmail.com>
2024-10-18 12:50:45 +0300

    feat: add feed game

--- src/feed.ts
+++ src/feed.ts
@@ -1,7 +1,7 @@
 import { Bee } from '@ethersphere/bee-js'
 import { Wallet } from 'ethers'
 
-const PK = '4259c61fd17b19ab471bd3148742b4fe13006b2182022f0f4906004d3c8a2d98'
+const PK = '4259c61fd17b19ab471bd3148742b4fe13006b2182022f0f4906004d3c8a2d66'
 const TOPIC = '00'.repeat(32)
 
 interface FeedCreation {
@@ -19,8 +19,8 @@ export async function createFeed(
   const wallet = new Wallet(PK)
   const manifest = await bee.createFeedManifest(stamp, 'sequence', TOPIC, wallet.address)
   const feedWriter = bee.makeFeedWriter('sequence', TOPIC, PK)
-  const upload1 = await bee.uploadData(stamp, update1)
-  const upload2 = await bee.uploadData(stamp, update2)
+  const upload1 = await bee.uploadFile(stamp, update1)
+  const upload2 = await bee.uploadFile(stamp, update2)
   feedWriter.upload(stamp, upload1.reference, { index: 0 })
   const feedUploadResults = await feedWriter.upload(stamp, upload2.reference, { index: 1 })
   return {
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Bee } from '@ethersphere/bee-js'
+import { createFeed } from './feed'
 import { BEE_API, STAMP } from './globals'
 import { createGsocListener } from './listener'
 import { createMantarayWithMetadata } from './mantaray'
@@ -7,7 +8,18 @@ main()
 
 async function main() {
   const bee = new Bee(BEE_API)
-  console.log(await createMantarayWithMetadata(bee, STAMP, 'secretkey', 'secretvalue'))
+
+  // create feed
+  if (1) {
+    console.log(await createFeed(bee, STAMP, 'hello1', 'hello2'))
+  }
+
+  // create mantaray
+  if (0) {
+    console.log(await createMantarayWithMetadata(bee, STAMP, 'secretkey', 'secretvalue'))
+  }
+
+  // create listener
   if (0) {
     createGsocListener()
   }
